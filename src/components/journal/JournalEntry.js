da099c937ba81896d45c4441210e0c3c370edbf5
Jeanpier Mendoza <jeanpiermendoza@outlook.com>
2021-06-05 22:18:07 -0500

    estilos y cascarón principal terminado

---
+++ src/components/journal/JournalEntry.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export const JournalEntry = ({}) => {
+  return (
+    <div className="journal__entry">
+      <div
+        className="journal__entry-picture"
+        style={{
+          backgroundSize: "cover",
+          backgroundImage:
+            "url(https://i.pinimg.com/originals/83/f9/37/83f937b69f30bb886ab8a03390da6771.jpg)",
+        }}
+      ></div>
+      <div className="journal__entry-body">
+        <p className="journal__entry-title">Un nuevo día</p>
+        <p className="journal__entry-content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit quo
+          veniam alias maxime corporis laboriosam.
+        </p>
+      </div>
+      <div className="journal__entry-date-box">
+        <span>Monday</span>
+        <h4>28</h4>
+      </div>
+    </div>
+  );
+};
