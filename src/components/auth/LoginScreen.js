2085d48ef75ae4837155b59909eaea86a8af1acc
Jeanpier Mendoza <jeanpiermendoza@outlook.com>
2021-06-19 18:54:49 -0500

    .

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -12,8 +12,8 @@ export const LoginScreen = () => {
   const { loading } = useSelector((state) => state.ui);
   const dispatch = useDispatch();
   const [formValues, handleInputChange] = useForm({
-    email: "jeanpier@mendoza.com",
-    password: "123456",
+    email: "",
+    password: "",
   });
   const { email, password } = formValues;
 
